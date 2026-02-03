b92c2459088876d921de4ae113313ce5a119eba3
Abderrahmane TAKI <rramaneabde@gmail.com>
2026-02-03 16:28:07 +0100

    03-Logic

---
+++ Javascript/03-Logic/solution.js
@@ -0,0 +1,10 @@
+let  walletBalance = 150
+let productPrice = 200
+
+if (walletBalance >= productPrice){
+    console.log("Successful");
+    
+}else {
+    console.log("Not enough money");
+    
+}
